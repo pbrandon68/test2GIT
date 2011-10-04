d318dd40791efed01124e678ac486bfa4f21e4f0
Paul Brandon <pjbrandon@comcast.net>
2011-10-04 19:43:24 -0400

    Test to see if we can use github

--- test2GIT/PsAppDelegate.h
+++ test2GIT/PsAppDelegate.h
@@ -5,7 +5,8 @@
 //  Created by Paul Brandon on 10/3/11.
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
-
+// Test of github
+//
 #import <UIKit/UIKit.h>
 
 @interface PsAppDelegate : UIResponder <UIApplicationDelegate>
